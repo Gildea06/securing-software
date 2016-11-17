ae3ffd3bb0ccaa3cbb7fe66926e21f4c48392e5a
Calvin Low <calsaviour@gmail.com>
2016-11-17 21:10:03 +0800

    Add solution Tasks

--- Set1-06.Notebook/src/main/java/sec/notebook/NotebookController.java
+++ Set1-06.Notebook/src/main/java/sec/notebook/NotebookController.java
@@ -1,7 +1,11 @@
 package sec.notebook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.sun.javafx.sg.prism.NGShape;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,5 +13,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NotebookController {
+    private List<String> data;
 
+    public NotebookController() {
+        this.data = new ArrayList<>();
+        //this.data.add("Hello world!");
+    }
+
+    @RequestMapping(value = "/")
+    public String home(Model model, @RequestParam(required = false) String content) {
+        if (content != null && !content.trim().isEmpty()) {
+            this.data.add(content);
+        }
+
+        model.addAttribute("list", data);
+
+        if (this.data.size() > 10) {
+            HashMap map = (HashMap) model.asMap();
+            List<String> mapList = (List<String>) map.get("list");
+            int originalSize = this.data.size() - 1;
+            List<String> topTen = new ArrayList();
+            for (int index = originalSize; index > originalSize - 10; index--) {
+                topTen.add(mapList.get(index));
+            }
+            model.addAttribute("list", topTen);
+        }
+
+        return "index";
+    }
 }
