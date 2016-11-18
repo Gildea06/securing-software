c0ee6f31ebb1744d449aca8015d271992bba09ec
Calvin Low <calsaviour@gmail.com>
2016-11-18 11:08:18 +0800

    Add solution HelloMessageRepository

--- Set2-04.HelloWebWithDatabase/src/main/java/sec/helloweb/HelloWebWithDatabaseController.java
+++ Set2-04.HelloWebWithDatabase/src/main/java/sec/helloweb/HelloWebWithDatabaseController.java
@@ -2,6 +2,7 @@ package sec.helloweb;
 
 import java.util.Collections;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,12 @@ public class HelloWebWithDatabaseController {
     @RequestMapping("/")
     @ResponseBody
     public String home() {
+        List<HelloMessage> messages = helloMessageRepository.findAll();
+        if (messages.size() > 0) {
+            HelloMessage message = messages.get(0);
+            helloMessageRepository.delete(message);
+            return message.getContent();
+        }
         return "Hello Web!";
     }
 }
