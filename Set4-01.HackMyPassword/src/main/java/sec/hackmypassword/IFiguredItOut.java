67bc44758f8e66748c97eddf7589a2cf846b834e
Calvin Low <calsaviour@gmail.com>
2016-11-28 18:18:41 +0800

    Add solution for HackMyPassword

--- Set4-01.HackMyPassword/src/main/java/sec/hackmypassword/IFiguredItOut.java
+++ Set4-01.HackMyPassword/src/main/java/sec/hackmypassword/IFiguredItOut.java
@@ -6,6 +6,6 @@ public class IFiguredItOut {
         // Do not change the class or the method name or the method signatures
         // Change the :( to the password that you found and return the 
         // application to the server.
-        return ":(";
+        return "president";
     }
 }
